f49f9319e2493483df5404d8d526b75fbd35c3f1
Gennadii Plahonin <plahonin99@mail.ru>
2022-08-12 17:34:10 +0900

    Added POJO cat, check for correction work with Scope

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld hwBean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hwBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("hwBean1 == hwBean2? " + hwBean1.equals(hwBean2));
+        System.out.println("catBean1 == catBean2? " + catBean1.equals(catBean2));
     }
 }
